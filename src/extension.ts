9ca8a3efc2d5479f72b692f471259e6f10f3f067
Jonathan Cammisuli <Jonathan.Cammisuli@soti.net>
2016-03-16 16:03:16 -0400

    added checkin, undo, get latest

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import {TfsCommand} from './tfsCommand'
+import * as utils from './utils';
 
 
 // this method is called when your extension is activated
@@ -12,8 +13,8 @@ export function activate(context: vscode.ExtensionContext) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "TFS-VSC" is now active!');
-    
-    
+
+
     // // The command has been defined in the package.json file
     // // Now provide the implementation of the command with  registerCommand
     // // The commandId parameter must match the command field in package.json
@@ -25,9 +26,23 @@ export function activate(context: vscode.ExtensionContext) {
     // });
 
     let tfsCommand = new TfsCommand();
-   
+
+    var disposables = [
+        vscode.commands.registerCommand('tfs-vsc.checkIn', () => {
+            tfsCommand.checkIn();
+        }),
+        vscode.commands.registerCommand('tfs-vsc.undo', () => {
+            tfsCommand.undo(utils.getCurrentFilePath());
+        }),
+        vscode.commands.registerCommand('tfs-vsc.getLatest', () => {
+            tfsCommand.getLatest();
+        })
+        // vscode.commands.registerCommand('tfs-vsc.list', tfsList),
+        // vscode.commands.registerCommand('tfs-vsc.status', function() { tfsExec('status', true); })
+    ];
+
     context.subscriptions.push(tfsCommand);
-    //context.subscriptions.push(disposable);
+    context.subscriptions.concat(disposables);
 }
 
 // this method is called when your extension is deactivated
--- src/tfscommand.ts
+++ src/tfscommand.ts
@@ -1,54 +1,95 @@
-import {workspace, window, Disposable} from 'vscode';
+import {workspace, window, Disposable, OutputChannel} from 'vscode';
 import * as utils from './utils';
-import {exec} from 'child_process';
+import {exec, spawnSync, spawn, execFile} from 'child_process';
+import {resolve} from 'path';
 
 export class TfsCommand {
 
     private _disposable: Disposable;
 
-    private _tfPath = "C:/Program Files (x86)/Microsoft Visual Studio 12.0/Common7/IDE/TF.exe"
+    private _tasksPath: string;
 
+    private _tfPath = '"C:/Program Files (x86)/Microsoft Visual Studio 12.0/Common7/IDE/TF.exe"';
+
+    private _outputWindow: OutputChannel;
+    
+    private _fileReverted = false;
 
     constructor() {
-        let subscriptions: Disposable[] = [];
+        
+        this._outputWindow = window.createOutputChannel("TFS");
 
+        let subscriptions: Disposable[] = [];
         workspace.onDidChangeTextDocument(this._onTextChange, this, subscriptions);
 
         this._disposable = Disposable.from(...subscriptions);
-
     }
 
     dispose() {
         this._disposable.dispose();
     }
 
+    getLatest() {
+        let cmd = `${this._tfPath} get "${workspace.rootPath}" /recursive`;
+        exec(cmd, (error, stdOut, stdErr) => {
+            window.showInformationMessage("Latest files retrieved.");
+            this._appendOutput("Finished getting all files from TFS");
+        });
+        
+        this._appendOutput("Getting all files from TFS");
+    }
+
+    checkIn() {
+        var child = exec(`start cmd.exe @cmd /k "${this._tfPath} checkin"`, function() { });
+        this._appendOutput("Checking in files.");
+    }
+
     checkOut(filePath: string) {
-        this.checkStatus(filePath).then(val => {
-            if (val) {
-                let cmd = `"${this._tfPath}" checkout "${filePath}"`;
+        this.isFileCheckedOut(filePath).then(checkedOut => {
+             if (!checkedOut) {
+                this._appendOutput("Checking out file: " + filePath);
+                
+                let cmd = `${this._tfPath} checkout "${filePath}"`;
                 exec(cmd, (error, stdOut, stdErr) => {
                     window.showInformationMessage("File checked out.");
+                    this._appendOutput("Checked out file: " + filePath);
                 });
             }
         });
     }
 
-    checkStatus(filePath: string): Promise<Boolean> {
-        let cmd = `"${this._tfPath}" status "${filePath}"`;
+    undo(filePath: string) {
+        let cmd = `${this._tfPath} undo "${filePath}"`;
+        this.isFileCheckedOut(filePath).then(checkedOut => {
+            this._appendOutput("Undoing file: " + filePath);
+            if (checkedOut) {
+                exec(cmd, (error, stdOut, stdErr) => {
+                    window.showInformationMessage("File reverted.");
+                    this._appendOutput("File reverted: " + filePath);
+                    this._fileReverted = true;
+                });
+            } else {
+                window.showInformationMessage("This file has no pending changes.");
+            }
+        });
+    }
+
+    isFileCheckedOut(filePath: string): Promise<Boolean> {
+        let cmd = `${this._tfPath} status "${filePath}"`;
 
         let promise = new Promise((resolve, reject) => {
             exec(cmd, (error, stdOut, stdErr) => {
                 if (error) {
                     reject(utils.getBufferContents(error));
                 }
-                if (stdErr && !stdOut) { // @note: stderr can be getted with stdout at one time!
+                if (stdErr && !stdOut) {
                     reject(utils.getBufferContents(stdErr));
                 }
                 let responseString = stdOut.toString('utf8').replace(/\r?\n|\r/, '');
                 if (responseString == "There are no pending changes.") {
-                    resolve(true);
-                } else {
                     resolve(false);
+                } else {
+                    resolve(true);
                 }
             });
         });
@@ -62,8 +103,17 @@ export class TfsCommand {
         }
 
         if (!_.isEmpty(utils.getCurrentFilePath())) {
-            this.checkOut(utils.getCurrentFilePath());
+            
+            if (!this._fileReverted) {
+                this.checkOut(utils.getCurrentFilePath());
+            }
+            
+            this._fileReverted = false;
         }
+    }
+    
+    private _appendOutput(value: string) {
+        this._outputWindow.appendLine(new Date().toLocaleString() + " " +value);
     }
 
 }
